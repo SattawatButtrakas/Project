cf95dbdf532fbe56f1f7b57a89c4e799eb6030c1
SattawatButtrakas <56030339@kmitl.ac.th>
2017-02-17 18:09:39 +0700

    Verify E-mail

--- www/controllers/SignupstudentControl.js
+++ www/controllers/SignupstudentControl.js
@@ -1,7 +1,9 @@
 angular.module('app')
 	.controller('SignupstudentControl',function($scope, $state, $ionicPopup) {
      console.log('Signupstudent START.');
-
+     $scope.RE = /^[a-zA-Zก-ฮ่้ัีิ์ืเาุู๊ ]{1,25}$/;
+     $scope.NE = /^[0-9 ]{1,8}$/;
+     $scope.Email = /^([0-9])\d+@([\bkmitl()?\b])+\.([a])+c+\.+th/;
     $scope.btnBack = function () {
     console.log('Back pressed');
     $state.go('SignupMenu');
